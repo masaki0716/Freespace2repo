089f84cbbd62f06695e509200befc5889c185393
ubuntuSEGAWA <masa.swim.07-16@ezweb.ne.jp>
2025-05-01 20:23:03 +0900

    3000

--- main_server.js
+++ main_server.js
@@ -5,7 +5,8 @@ const { OpenAI } = require("openai");
 require("dotenv").config(); // .env から API キーを読み込む
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 
